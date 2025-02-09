bfe10e316b52b3f3802bbe1982a23f407152323d
dhopegraphics <dhopegraphics@gmail.com>
2025-02-09 12:47:28 +0000

    add navbar component and implement custom elements for header and footer

---
+++ functions/navBarFooter.js
@@ -0,0 +1,24 @@
+class SpecialHeader extends HTMLElement {
+    connectedCallback() {
+      fetch("components/navbar.html") // Load navbar from file
+        .then(response => response.text())
+        .then(data => {
+          this.innerHTML = data;
+        })
+        .catch(error => console.error("Error loading navbar:", error));
+    }
+  }
+  
+  class SpecialFooter extends HTMLElement {
+    connectedCallback() {
+      fetch("components/footer.html") // Load navbar from file
+        .then(response => response.text())
+        .then(data => {
+          this.innerHTML = data;
+        })
+        .catch(error => console.error("Error loading navbar:", error));
+    }
+  }
+  
+  customElements.define("special-header", SpecialHeader);
+  customElements.define("special-footer", SpecialFooter);
